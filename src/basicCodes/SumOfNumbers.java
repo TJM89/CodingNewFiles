171fceba8c631a29768fd4bfd4ac9ed9cb36802b
TJM <tj.extremez@gmail.com>
2022-03-17 21:26:25 -0400

    newcode

---
+++ src/basicCodes/SumOfNumbers.java
@@ -0,0 +1,15 @@
+package basicCodes;
+
+public class SumOfNumbers {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		for (int val = 0; val <= 100; val++)
+
+			sum = sum + val;
+
+		System.out.println(sum);
+	}
+
+}
